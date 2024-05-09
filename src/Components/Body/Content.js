2e8b3570c48a2109d6bb35741fac9cad0e9206cf
desmondrichard <desmondrichard1955@gmail.com>
2024-05-09 15:07:06 +0530

    Updated UI

--- src/Components/Body/Content.js
+++ src/Components/Body/Content.js
@@ -5,11 +5,14 @@ import Form from 'react-bootstrap/Form';
 function Content() {
     return (
         <div className='divContent'>
-            <div className="flex-container">
-                <div className="flex-items"><Form.Check /></div>
-                <div className="flex-items">Play</div>
-                <div className="flex-items trash"><i className="bi bi-trash3 "></i></div>
-            </div>
+            <ul className="flex-container">
+                <li>
+                    <div className="flex-items"><Form.Check /></div>
+                    <div className="flex-items">Play</div>
+                    <div className="flex-items trash"><i className="bi bi-trash3 "></i></div>
+                </li>
+
+            </ul>
         </div>
     )
 }
