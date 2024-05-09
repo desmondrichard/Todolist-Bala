bad4acc80d9accbbccebba8e6b1e705ed031ba1e
desmondrichard <desmondrichard1955@gmail.com>
2024-05-09 15:38:43 +0530

    Modified UI

--- src/Components/Body/Content.js
+++ src/Components/Body/Content.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Content.css';
 import Form from 'react-bootstrap/Form';
 
 function Content() {
+
+
     return (
         <div className='divContent'>
-            <div className="flex-container">
-                <div className="flex-items"><Form.Check /></div>
-                <div className="flex-items">Play</div>
-                <div className="flex-items trash"><i className="bi bi-trash3 "></i></div>
-            </div>
+            <ul >
+                <li className="flex-container">
+                    <input type="checkbox" checked="checked" />
+                    <p style={{ marginTop: '15px' }}>Playing</p>
+                    <i className="bi bi-trash3 "></i>
+                </li>
+            </ul>
         </div>
     )
 }
