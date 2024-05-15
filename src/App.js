7617f5bef2d059d787c9756e3ff9f98ae8a8f88a
desmondrichard <desmondrichard1955@gmail.com>
2024-05-15 11:21:30 +0530

    completed GET using fetch

--- src/App.js
+++ src/App.js
@@ -5,25 +5,55 @@ import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 function App() {
 
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("todo_list"))
-  )
+  const [items, setItems] = useState([])
+  const API_URL = 'http://localhost:8000/items';
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true)
+
+  //GET Request:
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        console.log("status", response.statusText)
+        //for our reference:
+        // if (response.statusText !== 'OK') {
+        //   alert("Not OK")
+        // } else {
+        //   alert("OK")
+        // }
+        if (response.statusText !== 'OK') throw Error('Could not get items');
+        console.log("response", response)
+        const listItems = await response.json();
+        console.log("listItems", listItems);
+        setItems(listItems);
+        setFetchError(null)
+      } catch (error) {
+        setFetchError(error.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    setTimeout(() => {
+      (async () => await fetchItems())()
+    }, 3000)
+  }, [])
 
   function handleCheckBoxClick(id) {
     console.log("ID", id)
     const listItems = items.map((item) => (item.id === id ? { ...item, checked: !item.checked } : item))
     setItems(listItems)
-    localStorage.setItem("todo_list", JSON.stringify(listItems))
+    // localStorage.setItem("todo_list", JSON.stringify(listItems))
   }
 
   function handleDeleteClick(id) {
     console.log("Clicked Delete btn");
     const listItems = items.filter((item) => (item.id !== id))
     setItems(listItems)
-    localStorage.setItem("todo_list", JSON.stringify(listItems))
+    // localStorage.setItem("todo_list", JSON.stringify(listItems))
   }
 
   //Creating a new list item:
@@ -34,7 +64,7 @@ function App() {
     const addNewItem = { id, checked: false, label }
     const listItems = [...items, addNewItem]
     setItems(listItems)
-    localStorage.setItem("todo_list", JSON.stringify(listItems))
+    // localStorage.setItem("todo_list", JSON.stringify(listItems))
   })
 
   const handleSubmitForm = (e) => {
@@ -56,7 +86,11 @@ function App() {
       <div style={{ width: '75%', border: '1px dashed gray' }}>
         <Header title="Todo List App" />
         <Search newItem={newItem} setnewItem={setnewItem} handleSubmitForm={handleSubmitForm} handleSearchSubmit={handleSearchSubmit} searchItem={searchItem} setSearchItem={setSearchItem} />
-        <Content items={items.filter((i) => ((i.label).toLowerCase()).includes(searchItem.toLowerCase()))} handleCheckBoxClick={handleCheckBoxClick} handleDeleteClick={handleDeleteClick} />
+        <>
+          {isLoading && <p className='text-center'>Loading Data..</p>}
+          {fetchError && <p className='text-center'>{`Error:${fetchError}`}</p>}
+          {!isLoading && !fetchError && <Content items={items.filter((i) => ((i.label).toLowerCase()).includes(searchItem.toLowerCase()))} handleCheckBoxClick={handleCheckBoxClick} handleDeleteClick={handleDeleteClick} />}
+        </>
         <Footer items={items} />
       </div>
     </div>
--- src/Components/Body/Search.js
+++ src/Components/Body/Search.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
@@ -6,15 +6,17 @@ import Button from 'react-bootstrap/Button';
 import './Search.css';
 
 function Search({ newItem, setnewItem, handleSubmitForm, handleSearchSubmit, searchItem, setSearchItem }) {
+    const inputRef = useRef();
+
     return (
         <div className='my-4 searchParentDiv'>
 
             <Row className='mx-auto'>
                 <Form onSubmit={handleSubmitForm} className='formAddItem'>
-                        <label htmlFor='addItem' className='labelSearch'>Add Item</label>
-                        <Form.Control type="text" placeholder="add item here..." id='addItem' value={newItem} onChange={(e) => setnewItem(e.target.value)} />
-                
-                        <Button type="submit" variant="outline-success" className='button'>+</Button>  
+                    <label htmlFor='addItem' className='labelSearch'>Add Item</label>
+                    <Form.Control autoFocus type="text" placeholder="add item here..." id='addItem' value={newItem} onChange={(e) => setnewItem(e.target.value)} ref={inputRef} />
+
+                    <Button type="submit" onClick={() => { inputRef.current.focus() }} variant="outline-success" className='button'>+</Button>
                 </Form>
                 <Form onSubmit={handleSearchSubmit}>
                     <Col xs={12} sm={12} className='mt-3'>
